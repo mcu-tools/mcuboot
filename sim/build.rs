902d617687c0a1dc72ba2d5cda81cbaf2599cd01
David Brown <david.brown@linaro.org>
2017-05-05 09:37:41 -0600

    Add capabilities query
    Add a simple function to query the bootloader for capabilities.
    Ultimately, this API should be available to the running app, but the
    simulator can use this to determine what to test.

--- sim/build.rs
+++ sim/build.rs
@@ -10,6 +10,7 @@ fn main() {
     let mut conf = gcc::Config::new();
 
     conf.file("../boot/bootutil/src/loader.c");
+    conf.file("../boot/bootutil/src/caps.c");
     conf.file("../boot/bootutil/src/bootutil_misc.c");
     conf.file("csupport/run.c");
     conf.include("../boot/bootutil/include");
