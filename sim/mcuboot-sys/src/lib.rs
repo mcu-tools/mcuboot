ea0da39bece99820d3d4da6f942126d834268eff
David Brown <david.brown@linaro.org>
2017-07-12 09:56:16 -0600

    sim: Flatten 'area' module in mcuboot-sys
    It isn't necessary for the 'area' module to be exposed.  Re-export the
    two definitions from the crate at the top level, and make the module
    private.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

--- sim/mcuboot-sys/src/lib.rs
+++ sim/mcuboot-sys/src/lib.rs
@@ -2,6 +2,11 @@ extern crate libc;
 #[macro_use] extern crate log;
 extern crate simflash;
 
-pub mod area;
+mod area;
 pub mod c;
+
+// The API needs to be public, even though it isn't intended to be called by Rust code, but the
+// functions are exported to C code.
 pub mod api;
+
+pub use area::{AreaDesc, FlashId};
