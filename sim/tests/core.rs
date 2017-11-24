adefd27a93265307079b6e590ed4f7114d949897
Fabio Utzig <utzig@apache.org>
2017-11-24 08:11:05 -0200

    Add simple status write test without interruption
    This test marks the status write regions as a bad region,
    causing errors, but it is allowed to complete the swap. On
    restart the firmware in slot0 should be validated and work.
    
    Signed-off-by: Fabio Utzig <utzig@apache.org>

--- sim/tests/core.rs
+++ sim/tests/core.rs
@@ -27,3 +27,4 @@ sim_test!(revert_with_fails, make_image, run_revert_with_fails);
 sim_test!(perm_with_fails, make_image, run_perm_with_fails);
 sim_test!(perm_with_random_fails, make_image, run_perm_with_random_fails_5);
 sim_test!(norevert, make_image, run_norevert);
+sim_test!(status_write_fails_complete, make_image, run_with_status_fails_complete);
