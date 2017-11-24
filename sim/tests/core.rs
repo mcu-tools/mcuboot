eedcc450d60bce6379164a2ed5135cb52fc23bb8
Fabio Utzig <utzig@apache.org>
2017-11-24 10:48:52 -0200

    Add test with write status area fail and interruption
    Signed-off-by: Fabio Utzig <utzig@apache.org>

--- sim/tests/core.rs
+++ sim/tests/core.rs
@@ -28,3 +28,4 @@ sim_test!(perm_with_fails, make_image, run_perm_with_fails);
 sim_test!(perm_with_random_fails, make_image, run_perm_with_random_fails_5);
 sim_test!(norevert, make_image, run_norevert);
 sim_test!(status_write_fails_complete, make_image, run_with_status_fails_complete);
+sim_test!(status_write_fails_with_reset, make_image, run_with_status_fails_with_reset);
