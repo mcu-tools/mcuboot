28ee5b0ea93d8934c92cee8091e988828fb7c07e
Fabio Utzig <utzig@apache.org>
2017-12-12 08:10:40 -0200

    Add Zephyr support for bundled mbed-tls ASN1 parser
    When using EC256 for signing, Zephyr now uses the bundled tinycrypt
    and mbed-tls ASN1 parser instead of relying on the one provided by
    the OS.
    
    Signed-off-by: Fabio Utzig <utzig@apache.org>

--- boot/zephyr/os.c
+++ boot/zephyr/os.c
@@ -22,11 +22,12 @@
 
 #include "os/os_heap.h"
 
+#ifdef MCUBOOT_USE_MBED_TLS
+
 #define MBEDTLS_CONFIG_FILE CONFIG_MBEDTLS_CFG_FILE
 #include <mbedtls/platform.h>
 #include <mbedtls/memory_buffer_alloc.h>
 
-#ifdef MCUBOOT_USE_MBED_TLS
 /*
  * This is the heap for mbed TLS.  The value needed depends on the key
  * size and algorithm used.  For RSA-2048, 6144 bytes seems to be
