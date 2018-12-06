3b8b58dcb541caa19f60c70fd51902abbbebfc22
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-12-06 17:26:43 +0530

    Add DT_SPI_NOR_DRV_NAME check towards sanitycheck in target.h
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>

--- boot/zephyr/include/target.h
+++ boot/zephyr/include/target.h
@@ -33,7 +33,8 @@
 /*
  * Sanity check the target support.
  */
-#if !defined(DT_FLASH_DEV_NAME) || \
+#if (!defined(CONFIG_XTENSA) && !defined(DT_FLASH_DEV_NAME)) || \
+    (defined(CONFIG_XTENSA) && !defined(DT_SPI_NOR_DRV_NAME)) || \
     !defined(FLASH_ALIGN) ||                  \
     !defined(FLASH_AREA_IMAGE_0_OFFSET) || \
     !defined(FLASH_AREA_IMAGE_0_SIZE) || \
