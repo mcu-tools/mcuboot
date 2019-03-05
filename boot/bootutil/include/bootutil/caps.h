6c5a94569170cbbf0553f0f20ee2bf5fa2fe9bf5
David Brown <david.brown@linaro.org>
2019-03-05 12:13:33 -0700

    Add a capability query for image number
    In addition to the binary capability flags, add a query function that
    returns the number of images MCUboot has been configured to support.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

--- boot/bootutil/include/bootutil/caps.h
+++ boot/bootutil/include/bootutil/caps.h
@@ -41,6 +41,12 @@ uint32_t bootutil_get_caps(void);
 #define BOOTUTIL_CAP_ENC_KW                 (1<<6)
 #define BOOTUTIL_CAP_VALIDATE_PRIMARY_SLOT  (1<<7)
 
+/*
+ * Query the number of images this bootloader is configured for.  This
+ * is also primarily used for testing.
+ */
+uint32_t bootutil_get_num_images(void);
+
 #ifdef __cplusplus
 }
 #endif
