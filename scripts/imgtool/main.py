f13db126bddebeed4fbeddb53923f80d763cade2
Christian Skubich <christian@skubware.de>
2019-07-31 11:34:15 +0200

    bugfix imgtool verify()
    Signed-off-by: Christian Skubich <christian@skubware.de>

--- scripts/imgtool/main.py
+++ scripts/imgtool/main.py
@@ -116,12 +116,14 @@ def verify(key, imgfile):
         return
     elif ret == image.VerifyResult.INVALID_MAGIC:
         print("Invalid image magic; is this an MCUboot image?")
-    elif ret == image.VerifyResult.INVALID_MAGIC:
+    elif ret == image.VerifyResult.INVALID_TLV_INFO_MAGIC:
         print("Invalid TLV info magic; is this an MCUboot image?")
     elif ret == image.VerifyResult.INVALID_HASH:
         print("Image has an invalid sha256 digest")
     elif ret == image.VerifyResult.INVALID_SIGNATURE:
         print("No signature found for the given key")
+    else:
+        print("Unknown return code: {}".format(ret))
     sys.exit(1)
 
 
