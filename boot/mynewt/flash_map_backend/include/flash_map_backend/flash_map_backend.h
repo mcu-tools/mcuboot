8f927b8bce3286abfdf107b9bc6f37ac296b72b4
Fabio Utzig <utzig@apache.org>
2019-08-23 11:37:52 -0300

    Add basic multi-image support for Mynewt
    Allow Mynewt to be used in multi-image builds. Primary and secondary
    slots of image 1 are hard-coded to FLASH_AREA_IMAGE_2 and FLASH_AREA_IMAGE_3.
    
    Signed-off-by: Fabio Utzig <utzig@apache.org>

--- boot/mynewt/flash_map_backend/include/flash_map_backend/flash_map_backend.h
+++ boot/mynewt/flash_map_backend/include/flash_map_backend/flash_map_backend.h
@@ -7,7 +7,10 @@
 #ifndef __FLASH_MAP_BACKEND_H__
 #define __FLASH_MAP_BACKEND_H__
 
-#include <flash_map/flash_map.h>
+#include <sysflash/sysflash.h>
+#include <mcuboot_config/mcuboot_config.h>
+
+#if (MCUBOOT_IMAGE_NUMBER == 1)
 
 #define FLASH_AREA_IMAGE_PRIMARY(x)    (((x) == 0) ?          \
                                          FLASH_AREA_IMAGE_0 : \
@@ -15,6 +18,23 @@
 #define FLASH_AREA_IMAGE_SECONDARY(x)  (((x) == 0) ?          \
                                          FLASH_AREA_IMAGE_1 : \
                                          FLASH_AREA_IMAGE_1)
+
+#elif (MCUBOOT_IMAGE_NUMBER == 2)
+
+#define FLASH_AREA_IMAGE_PRIMARY(x)    (((x) == 0) ?          \
+                                         FLASH_AREA_IMAGE_0 : \
+                                        ((x) == 1) ?          \
+                                         FLASH_AREA_IMAGE_2 : \
+                                         255)
+#define FLASH_AREA_IMAGE_SECONDARY(x)  (((x) == 0) ?          \
+                                         FLASH_AREA_IMAGE_1 : \
+                                        ((x) == 1) ?          \
+                                         FLASH_AREA_IMAGE_3 : \
+                                         255)
+
+#else
+#error "Image slot and flash area mapping is not defined"
+#endif
 
 int flash_area_id_from_multi_image_slot(int image_index, int slot);
 int flash_area_id_to_multi_image_slot(int image_index, int area_id);
