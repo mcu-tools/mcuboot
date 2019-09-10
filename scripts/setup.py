9f857d06ee0eceef0876e2383b2324b4d60b01b0
Fabio Utzig <utzig@apache.org>
2019-09-10 12:52:26 -0300

    Add versioning to imgtool
    Signed-off-by: Fabio Utzig <utzig@apache.org>

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,8 +1,9 @@
 import setuptools
+from imgtool import imgtool_version
 
 setuptools.setup(
     name="imgtool",
-    version="1.3.1",
+    version=imgtool_version,
     author="The MCUboot commiters",
     description=("MCUboot's image signing and key management"),
     license="Apache Software License",
