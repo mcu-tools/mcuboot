60cc568615e9f34f90677c12bc4264a5dd802303
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2020-06-08 11:36:13 +0200

    Revert "zephyr: Change DT_FLASH_AREA_ macros to FLASH_AREA_"
    This reverts commit b7254d11cd305e321e48368a09795dd01c3856af.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- boot/zephyr/include/sysflash/sysflash.h
+++ boot/zephyr/include/sysflash/sysflash.h
@@ -13,32 +13,32 @@
  * running in single image mode.
  */
 #define FLASH_AREA_IMAGE_PRIMARY(x)    (((x) == 0) ?                \
-                                         FLASH_AREA_ID(image_0) : \
-                                         FLASH_AREA_ID(image_0))
+                                         DT_FLASH_AREA_IMAGE_0_ID : \
+                                         DT_FLASH_AREA_IMAGE_0_ID)
 #define FLASH_AREA_IMAGE_SECONDARY(x)  (((x) == 0) ?                \
-                                         FLASH_AREA_ID(image_1) : \
-                                         FLASH_AREA_ID(image_1))
+                                         DT_FLASH_AREA_IMAGE_1_ID : \
+                                         DT_FLASH_AREA_IMAGE_1_ID)
 #elif (MCUBOOT_IMAGE_NUMBER == 2)
 /* MCUBoot currently supports only up to 2 updateable firmware images.
  * If the number of the current image is greater than MCUBOOT_IMAGE_NUMBER - 1
  * then a dummy value will be assigned to the flash area macros.
  */
 #define FLASH_AREA_IMAGE_PRIMARY(x)    (((x) == 0) ?                \
-                                         FLASH_AREA_ID(image_0) : \
+                                         DT_FLASH_AREA_IMAGE_0_ID : \
                                         ((x) == 1) ?                \
-                                         FLASH_AREA_ID(image_2) : \
+                                         DT_FLASH_AREA_IMAGE_2_ID : \
                                          255)
 #define FLASH_AREA_IMAGE_SECONDARY(x)  (((x) == 0) ?                \
-                                         FLASH_AREA_ID(image_1) : \
+                                         DT_FLASH_AREA_IMAGE_1_ID : \
                                         ((x) == 1) ?                \
-                                         FLASH_AREA_ID(image_3) : \
+                                         DT_FLASH_AREA_IMAGE_3_ID : \
                                          255)
 #else
 #error "Image slot and flash area mapping is not defined"
 #endif
 
 #if !defined(CONFIG_BOOT_SWAP_USING_MOVE)
-#define FLASH_AREA_IMAGE_SCRATCH    FLASH_AREA_ID(image_scratch)
+#define FLASH_AREA_IMAGE_SCRATCH    DT_FLASH_AREA_IMAGE_SCRATCH_ID
 #endif
 
 #endif /* __SYSFLASH_H__ */
