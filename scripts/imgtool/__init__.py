1a1600b0cd27d5e004be344574901c64cdd6f7a2
Ihor Slabkyy <ihor.slabkyy@cypress.com>
2020-08-05 13:34:07 +0300

    Change imgtool version to 1.7.0a1
    Signed-off-by: Ihor Slabkyy <ihor.slabkyy@cypress.com>

--- scripts/imgtool/__init__.py
+++ scripts/imgtool/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Linaro Limited
+# Copyright 2017-2020 Linaro Limited
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-imgtool_version = "1.6.0"
+imgtool_version = "1.7.0a1"
