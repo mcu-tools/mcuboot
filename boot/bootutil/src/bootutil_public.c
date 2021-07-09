7be72df63d2fa2923b90dc7e5c4954a1f3ac6324
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2021-07-09 12:31:35 +0200

    bootutil_public: added hook for fetch image swap state
    Added hook which allows to override boot_read_swap_state_by_id()
    routine for the primary slot.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- boot/bootutil/src/bootutil_public.c
+++ boot/bootutil/src/bootutil_public.c
@@ -50,6 +50,8 @@
 #include "bootutil/enc_key_public.h"
 #endif
 
+#include "bootutil/boot_public_hooks.h"
+
 #ifdef CONFIG_MCUBOOT
 BOOT_LOG_MODULE_DECLARE(mcuboot);
 #else
@@ -418,8 +420,13 @@ boot_swap_type_multi(int image_index)
     int rc;
     size_t i;
 
-    rc = boot_read_swap_state_by_id(FLASH_AREA_IMAGE_PRIMARY(image_index),
-                                    &primary_slot);
+    rc = BOOT_HOOK_CALL(boot_read_swap_state_primary_slot_hook,
+                        BOOT_HOOK_REGULAR, image_index, &primary_slot);
+    if (rc == BOOT_HOOK_REGULAR)
+    {
+        rc = boot_read_swap_state_by_id(FLASH_AREA_IMAGE_PRIMARY(image_index),
+                                        &primary_slot);
+    }
     if (rc) {
         return BOOT_SWAP_TYPE_PANIC;
     }
