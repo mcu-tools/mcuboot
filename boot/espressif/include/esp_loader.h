6eea9ecfb2514e46a11de76ae20f742e67794028
Shubham Kulkarni <shubham.kulkarni@espressif.com>
2021-07-20 11:46:03 +0530

    boot/espressif: Update main.c to call init and loader functions
    Add ESP specific loader code to load host application
    
    Signed-off-by: Shubham Kulkarni <shubham.kulkarni@espressif.com>

---
+++ boot/espressif/include/esp_loader.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2021 Espressif Systems (Shanghai) Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+void esp_app_image_load(int slot, unsigned int hdr_offset);
