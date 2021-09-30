d969cd9938bc29734f2ce4b79d3f9f3c45750853
Dominik Ermel <dominik.ermel@nordicsemi.no>
2021-09-30 15:01:26 +0000

    zephyr: Fix support for single application with serial recovery
    The commit fixes compilation error when attempting to build
    support for single application slot with serial recovery enabled.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- boot/boot_serial/src/boot_serial.c
+++ boot/boot_serial/src/boot_serial.c
@@ -63,6 +63,10 @@
 
 BOOT_LOG_MODULE_DECLARE(mcuboot);
 
+#ifndef BOOT_IMAGE_NUMBER
+#define BOOT_IMAGE_NUMBER MCUBOOT_IMAGE_NUMBER
+#endif
+
 #define BOOT_SERIAL_INPUT_MAX   512
 #define BOOT_SERIAL_OUT_MAX     (128 * MCUBOOT_IMAGE_NUMBER)
 
@@ -75,10 +79,6 @@ BOOT_LOG_MODULE_DECLARE(mcuboot);
 
 #define ntohs(x) sys_be16_to_cpu(x)
 #define htons(x) sys_cpu_to_be16(x)
-#endif
-
-#ifndef BOOT_IMAGE_NUMBER
-#define BOOT_IMAGE_NUMBER MCUBOOT_IMAGE_NUMBER
 #endif
 
 #if (BOOT_IMAGE_NUMBER > 1)
