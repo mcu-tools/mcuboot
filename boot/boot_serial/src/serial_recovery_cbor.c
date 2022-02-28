390b99dfe60a9f50c1104f812db9b30ae4b05fd4
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2022-02-28 13:47:57 +0100

    boot_serial: Upgrade from cddl-gen 0.1.0 to zcbor 0.4.0
    cddl-gen has been renamed to zcbor.
    Update regenerate_serial_recovery_cbor.sh and regenerate/recopy all
    files.
    
    Remove the submodule in ext/ since it is no longer necessary when
    the zcbor package is installed (only needed for regeneration, not
    for building).
    
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- boot/boot_serial/src/serial_recovery_cbor.c
+++ boot/boot_serial/src/serial_recovery_cbor.c
@@ -1,25 +1,21 @@
 /*
- * This file has been generated from the cddl-gen submodule.
- * Commit 9f77837f9950da1633d22abf6181a830521a6688
- */
-
-/*
- * Copyright (c) 2021 Nordic Semiconductor ASA
+ * Copyright (c) 2022 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 /*
- * Generated with cddl_gen.py (https://github.com/NordicSemiconductor/cddl-gen)
- * at: 2021-08-02 17:09:42
- * Generated with a default_max_qty of 3
+ * Generated using zcbor version 0.4.0
+ * https://github.com/NordicSemiconductor/zcbor
+ * at: 2022-03-31 12:37:11
+ * Generated with a --default-max-qty of 3
  */
 
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <string.h>
-#include "cbor_decode.h"
+#include "zcbor_decode.h"
 #include "serial_recovery_cbor.h"
 
 #if DEFAULT_MAX_QTY != 3
@@ -28,67 +24,76 @@
 
 
 static bool decode_Member(
-		cbor_state_t *state, struct Member_ *result)
+		zcbor_state_t *state, struct Member_ *result)
 {
-	cbor_print("%s\n", __func__);
-	cbor_string_type_t tmp_str;
+	zcbor_print("%s\r\n", __func__);
+	struct zcbor_string tmp_str;
 	bool int_res;
 
-	bool tmp_result = (((union_start_code(state) && (int_res = (((((tstrx_expect(state, ((tmp_str.value = (const uint8_t *)"image",
-    tmp_str.len = sizeof("image") - 1, &tmp_str)))))
-	&& (intx32_decode(state, (&(*result)._Member_image)))) && (((*result)._Member_choice = _Member_image) || 1))
-	|| (union_elem_code(state) && ((((tstrx_expect(state, ((tmp_str.value = (const uint8_t *)"data",
-    tmp_str.len = sizeof("data") - 1, &tmp_str)))))
-	&& (bstrx_decode(state, (&(*result)._Member_data)))) && (((*result)._Member_choice = _Member_data) || 1)))
-	|| (union_elem_code(state) && ((((tstrx_expect(state, ((tmp_str.value = (const uint8_t *)"len",
-    tmp_str.len = sizeof("len") - 1, &tmp_str)))))
-	&& (intx32_decode(state, (&(*result)._Member_len)))) && (((*result)._Member_choice = _Member_len) || 1)))
-	|| (union_elem_code(state) && ((((tstrx_expect(state, ((tmp_str.value = (const uint8_t *)"off",
-    tmp_str.len = sizeof("off") - 1, &tmp_str)))))
-	&& (intx32_decode(state, (&(*result)._Member_off)))) && (((*result)._Member_choice = _Member_off) || 1)))
-	|| (union_elem_code(state) && ((((tstrx_expect(state, ((tmp_str.value = (const uint8_t *)"sha",
-    tmp_str.len = sizeof("sha") - 1, &tmp_str)))))
-	&& (bstrx_decode(state, (&(*result)._Member_sha)))) && (((*result)._Member_choice = _Member_sha) || 1)))), union_end_code(state), int_res))));
+	bool tmp_result = (((zcbor_union_start_code(state) && (int_res = (((((zcbor_tstr_expect(state, ((tmp_str.value = (uint8_t *)"image", tmp_str.len = sizeof("image") - 1, &tmp_str)))))
+	&& (zcbor_int32_decode(state, (&(*result)._Member_image)))) && (((*result)._Member_choice = _Member_image) || 1))
+	|| (zcbor_union_elem_code(state) && ((((zcbor_tstr_expect(state, ((tmp_str.value = (uint8_t *)"data", tmp_str.len = sizeof("data") - 1, &tmp_str)))))
+	&& (zcbor_bstr_decode(state, (&(*result)._Member_data)))) && (((*result)._Member_choice = _Member_data) || 1)))
+	|| (zcbor_union_elem_code(state) && ((((zcbor_tstr_expect(state, ((tmp_str.value = (uint8_t *)"len", tmp_str.len = sizeof("len") - 1, &tmp_str)))))
+	&& (zcbor_int32_decode(state, (&(*result)._Member_len)))) && (((*result)._Member_choice = _Member_len) || 1)))
+	|| (zcbor_union_elem_code(state) && ((((zcbor_tstr_expect(state, ((tmp_str.value = (uint8_t *)"off", tmp_str.len = sizeof("off") - 1, &tmp_str)))))
+	&& (zcbor_int32_decode(state, (&(*result)._Member_off)))) && (((*result)._Member_choice = _Member_off) || 1)))
+	|| (zcbor_union_elem_code(state) && ((((zcbor_tstr_expect(state, ((tmp_str.value = (uint8_t *)"sha", tmp_str.len = sizeof("sha") - 1, &tmp_str)))))
+	&& (zcbor_bstr_decode(state, (&(*result)._Member_sha)))) && (((*result)._Member_choice = _Member_sha) || 1)))), zcbor_union_end_code(state), int_res))));
 
 	if (!tmp_result)
-		cbor_trace();
+		zcbor_trace();
+
+	return tmp_result;
+}
+
+static bool decode_repeated_Upload_members(
+		zcbor_state_t *state, struct Upload_members *result)
+{
+	zcbor_print("%s\r\n", __func__);
+
+	bool tmp_result = (((decode_Member(state, (&(*result)._Upload_members)))));
+
+	if (!tmp_result)
+		zcbor_trace();
 
 	return tmp_result;
 }
 
 static bool decode_Upload(
-		cbor_state_t *state, struct Upload *result)
+		zcbor_state_t *state, struct Upload *result)
 {
-	cbor_print("%s\n", __func__);
-	bool int_res;
+	zcbor_print("%s\r\n", __func__);
 
-	bool tmp_result = (((map_start_decode(state) && (int_res = (multi_decode(1, 5, &(*result)._Upload_members_count, (void *)decode_Member, state, (&(*result)._Upload_members), sizeof(struct Member_))), ((map_end_decode(state)) && int_res)))));
+	bool tmp_result = (((zcbor_map_start_decode(state) && ((zcbor_multi_decode(1, 5, &(*result)._Upload_members_count, (zcbor_decoder_t *)decode_repeated_Upload_members, state, (&(*result)._Upload_members), sizeof(struct Upload_members))) || (zcbor_list_map_end_force_decode(state), false)) && zcbor_map_end_decode(state))));
 
 	if (!tmp_result)
-		cbor_trace();
+		zcbor_trace();
 
 	return tmp_result;
 }
 
 
 
-__attribute__((unused)) static bool type_test_decode_Upload(
-		struct Upload *result)
-{
-	/* This function should not be called, it is present only to test that
-	 * the types of the function and struct match, since this information
-	 * is lost with the casts in the entry function.
-	 */
-	return decode_Upload(NULL, result);
-}
-
-
-bool cbor_decode_Upload(
-		const uint8_t *payload, uint32_t payload_len,
+int cbor_decode_Upload(
+		const uint8_t *payload, size_t payload_len,
 		struct Upload *result,
-		uint32_t *payload_len_out)
+		size_t *payload_len_out)
 {
-	return entry_function(payload, payload_len, (const void *)result,
-		payload_len_out, (void *)decode_Upload,
-		1, 2);
+	zcbor_state_t states[4];
+
+	zcbor_new_state(states, sizeof(states) / sizeof(zcbor_state_t), payload, payload_len, 1);
+
+	bool ret = decode_Upload(states, result);
+
+	if (ret && (payload_len_out != NULL)) {
+		*payload_len_out = MIN(payload_len,
+				(size_t)states[0].payload - (size_t)payload);
+	}
+
+	if (!ret) {
+		int ret = zcbor_pop_error(states);
+		return (ret == ZCBOR_SUCCESS) ? ZCBOR_ERR_UNKNOWN : ret;
+	}
+	return ZCBOR_SUCCESS;
 }
--- boot/boot_serial/src/serial_recovery_cbor.h
+++ boot/boot_serial/src/serial_recovery_cbor.h
@@ -1,18 +1,14 @@
 /*
- * This file has been generated from the cddl-gen submodule.
- * Commit 9f77837f9950da1633d22abf6181a830521a6688
- */
-
-/*
- * Copyright (c) 2021 Nordic Semiconductor ASA
+ * Copyright (c) 2022 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 /*
- * Generated with cddl_gen.py (https://github.com/NordicSemiconductor/cddl-gen)
- * at: 2021-08-02 17:09:42
- * Generated with a default_max_qty of 3
+ * Generated using zcbor version 0.4.0
+ * https://github.com/NordicSemiconductor/zcbor
+ * at: 2022-03-31 12:37:11
+ * Generated with a --default-max-qty of 3
  */
 
 #ifndef SERIAL_RECOVERY_CBOR_H__
@@ -22,18 +18,18 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <string.h>
-#include "cbor_decode.h"
-#include "types_serial_recovery_cbor.h"
+#include "zcbor_decode.h"
+#include "serial_recovery_cbor_types.h"
 
 #if DEFAULT_MAX_QTY != 3
 #error "The type file was generated with a different default_max_qty than this file"
 #endif
 
 
-bool cbor_decode_Upload(
-		const uint8_t *payload, uint32_t payload_len,
+int cbor_decode_Upload(
+		const uint8_t *payload, size_t payload_len,
 		struct Upload *result,
-		uint32_t *payload_len_out);
+		size_t *payload_len_out);
 
 
 #endif /* SERIAL_RECOVERY_CBOR_H__ */
