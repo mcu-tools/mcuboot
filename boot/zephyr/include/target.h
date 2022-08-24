3a82b6ff1eba876346c67a08cebe3bddd7d3278e
Dominik Ermel <dominik.ermel@nordicsemi.no>
2022-08-24 14:35:24 +0000

    zephyr: Switch to using FIXED_PARTITION_ macros
    The FLASH_AREA_ macros, which have been using DTS node label property
    to identify partitions, have been replaced with FIXED_PARTITION_
    macros that use DTS node label to identify partitions.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- boot/zephyr/include/target.h
+++ boot/zephyr/include/target.h
@@ -33,14 +33,14 @@
 #if (!defined(CONFIG_XTENSA) && !DT_HAS_CHOSEN(zephyr_flash_controller)) || \
     (defined(CONFIG_XTENSA) && !DT_NODE_EXISTS(DT_INST(0, jedec_spi_nor))) || \
     !defined(FLASH_ALIGN) ||                  \
-    !(FLASH_AREA_LABEL_EXISTS(image_0)) || \
-    !(FLASH_AREA_LABEL_EXISTS(image_1) || CONFIG_SINGLE_APPLICATION_SLOT) || \
-    (defined(CONFIG_BOOT_SWAP_USING_SCRATCH) && !FLASH_AREA_LABEL_EXISTS(image_scratch))
+    !(FIXED_PARTITION_EXISTS(slot0_partition)) || \
+    !(FIXED_PARTITION_EXISTS(slot1_partition) || CONFIG_SINGLE_APPLICATION_SLOT) || \
+    (defined(CONFIG_BOOT_SWAP_USING_SCRATCH) && !FIXED_PARTITION_EXISTS(scratch_partition))
 #error "Target support is incomplete; cannot build mcuboot."
 #endif
 
-#if (MCUBOOT_IMAGE_NUMBER == 2) && (!(FLASH_AREA_LABEL_EXISTS(image_2)) || \
-                                     !(FLASH_AREA_LABEL_EXISTS(image_3)))
+#if (MCUBOOT_IMAGE_NUMBER == 2) && (!(FIXED_PARTITION_EXISTS(slot2_partition)) || \
+                                     !(FIXED_PARTITION_EXISTS(slot3_partition)))
 #error "Target support is incomplete; cannot build mcuboot."
 #endif
 
