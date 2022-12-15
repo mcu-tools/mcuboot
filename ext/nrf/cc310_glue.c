5ac53fa622a7c6410939059231f0ca01ce5a32f3
Dominik Kilian <Dominik.Kilian@nordicsemi.no>
2022-12-15 15:37:19 +0100

    boot: Update Nordic's license identifier tag
    Nordic had changed its license identifier to new
    more accurate id: LicenseRef-Nordic-5-Clause.
    Old identifiers should be updated.
    
    Signed-off-by: Dominik Kilian <Dominik.Kilian@nordicsemi.no>

--- ext/nrf/cc310_glue.c
+++ ext/nrf/cc310_glue.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2019 Nordic Semiconductor ASA
  *
- * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
 #include "cc310_glue.h"
--- ext/nrf/cc310_glue.h
+++ ext/nrf/cc310_glue.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2019 Nordic Semiconductor ASA
  *
- * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 #ifndef NRF_CC310_GLUE_H__
 #define NRF_CC310_GLUE_H__
