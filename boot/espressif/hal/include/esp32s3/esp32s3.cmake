4c8bbd2a1b0f1da38874e249bd9945962a38aabb
Almir Okato <almir.okato@espressif.com>
2023-01-20 00:57:21 -0300

    espressif:ESP32-S3: Fix multiboot APP CPU start
    Add missing function for starting the APP CPU when booting the
    second image (multi image).
    
    Signed-off-by: Almir Okato <almir.okato@espressif.com>

--- boot/espressif/hal/include/esp32s3/esp32s3.cmake
+++ boot/espressif/hal/include/esp32s3/esp32s3.cmake
@@ -6,6 +6,12 @@ list(APPEND hal_srcs
     ${esp_idf_dir}/components/efuse/src/esp_efuse_api_key_esp32xx.c
 )
 
+if (DEFINED CONFIG_ESP_MULTI_PROCESSOR_BOOT)
+    list(APPEND hal_srcs
+        ${src_dir}/${MCUBOOT_TARGET}/app_cpu_start.c
+        )
+endif()
+
 list(APPEND LINKER_SCRIPTS
     -T${esp_idf_dir}/components/esp_rom/${MCUBOOT_TARGET}/ld/${MCUBOOT_TARGET}.rom.newlib.ld
     )
