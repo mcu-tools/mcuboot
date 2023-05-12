110036acc973f1915ccadacbdbee1b429353f1cd
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2023-05-12 10:46:04 +0100

    boot: zcbor: Move copy script
    Moves and updates the copy script to the zcbor folder
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

---
+++ boot/zcbor/add_zcbor_copy_version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$1" == "--help" ]; then
+	echo "Add header if the zcbor files are updated."
+	exit -1
+fi
+
+add_copy_notice() {
+echo "$(printf '/*
+ * This file has been %s from the zcbor library.
+ * Commit %s
+ */
+
+' "$2" "$(zcbor --version)"; cat $1;)" > $1
+}
+
+add_copy_notice src/zcbor_decode.c "copied"
+add_copy_notice src/zcbor_encode.c "copied"
+add_copy_notice src/zcbor_common.c "copied"
+add_copy_notice include/zcbor_decode.h "copied"
+add_copy_notice include/zcbor_encode.h "copied"
+add_copy_notice include/zcbor_common.h "copied"
