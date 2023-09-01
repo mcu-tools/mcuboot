83e524c2b04120e8fc4172c9ef7595dd879ccc71
Francois Ramu <francois.ramu@st.com>
2023-09-01 10:20:40 +0200

    boot: zephyr: defines FLASH device for external NOR
    With CONFIG_STM32_MEMMAP flag get NOR flash 32 bytes header
    Read the NOR flash to get header of the image.
    The FLASH_DEVICE is now the external NOR defined by
    the board device tree to be the first qspi/ospi/xspi instance
    (not necessarily zephyr, flash-controller)
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>
    Signed-off-by: Erwan Gouriou <erwan.gouriou@st.com>

--- boot/zephyr/flash_map_extended.c
+++ boot/zephyr/flash_map_extended.c
@@ -20,7 +20,24 @@
 
 BOOT_LOG_MODULE_DECLARE(mcuboot);
 
-#if (!defined(CONFIG_XTENSA) && DT_HAS_CHOSEN(zephyr_flash_controller))
+#if defined(CONFIG_STM32_MEMMAP)
+/* MEMORY MAPPED for XiP on external NOR flash takes the sspi-nor or ospi-nor or qspi-nor device */
+#define FLASH_DEVICE_ID SPI_FLASH_0_ID
+#if DT_NODE_HAS_STATUS(DT_INST(0, st_stm32_xspi_nor), okay)
+#define DT_DRV_COMPAT st_stm32_xspi_nor
+#define FLASH_DEVICE_NODE DT_INST(0, st_stm32_xspi_nor)
+#define FLASH_DEVICE_BASE DT_REG_ADDR_BY_IDX(DT_INST_PARENT(0), 1)
+#elif DT_NODE_HAS_STATUS(DT_INST(0, st_stm32_ospi_nor), okay)
+#define FLASH_DEVICE_NODE DT_INST(0, st_stm32_ospi_nor)
+#define FLASH_DEVICE_BASE DT_REG_ADDR(DT_INST(0, st_stm32_ospi_nor))
+#elif DT_NODE_HAS_STATUS(DT_INST(0, st_stm32_qspi_nor), okay)
+#define FLASH_DEVICE_NODE DT_INST(0, st_stm32_qspi_nor)
+#define FLASH_DEVICE_BASE DT_REG_ADDR(DT_INST(0, st_stm32_qspi_nor))
+#else
+#error "FLASH_DEVICE_NODE could not be determined"
+#endif
+
+#elif (!defined(CONFIG_XTENSA) && DT_HAS_CHOSEN(zephyr_flash_controller))
 #define FLASH_DEVICE_ID SOC_FLASH_0_ID
 #define FLASH_DEVICE_BASE CONFIG_FLASH_BASE_ADDRESS
 #define FLASH_DEVICE_NODE DT_CHOSEN(zephyr_flash_controller)
