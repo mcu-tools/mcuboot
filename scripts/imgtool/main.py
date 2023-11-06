172493d4c451feb80e39fa668581b960a51c5305
Andrej Butok <andrey.butok@nxp.com>
2023-11-06 15:01:19 +0100

    doc: imgtool: update align description
    Update the --align option values.
    Add its description.
    Delete [required], as it is optional now.
    
    Signed-off-by: Andrej Butok <andrey.butok@nxp.com>

--- scripts/imgtool/main.py
+++ scripts/imgtool/main.py
@@ -377,7 +377,8 @@ class BasedIntParamType(click.ParamType):
               'keyword to automatically generate it from the image version.')
 @click.option('-v', '--version', callback=validate_version,  required=True)
 @click.option('--align', type=click.Choice(['1', '2', '4', '8', '16', '32']),
-              required=False)
+              required=False,
+              help='Alignment used by swap update modes.')
 @click.option('--max-align', type=click.Choice(['8', '16', '32']),
               required=False,
               help='Maximum flash alignment. Set if flash alignment of the '
