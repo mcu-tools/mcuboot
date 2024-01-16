mcu-tools/mcuboot

commit 25b7c7a8e7322503358b1f68e3d62f4453847748

imgtool: make "align" command line parameter optional
Align parameter should be optional:
- it has a default value.
- it is not used for non-swap update modes.

Signed-off-by: Andrej Butok <andrey.butok@nxp.com>

scripts/imgtool/main.py
@@ -377,7 +377,7 @@ class BasedIntParamType(click.ParamType):
               'keyword to automatically generate it from the image version.')
 @click.option('-v', '--version', callback=validate_version,  required=True)
 @click.option('--align', type=click.Choice(['1', '2', '4', '8', '16', '32']),
-              required=True)
+              required=False)
 @click.option('--max-align', type=click.Choice(['8', '16', '32']),
               required=False,
               help='Maximum flash alignment. Set if flash alignment of the '

commit 2f7c5f2bd66f80ec4da1160d12ac7bf577da7a75

imgtool: fix "align" script error
Fix "align" possible script error, caused by #1833.
Set default value for the align parameter.

Signed-off-by: Andrej Butok <andrey.butok@nxp.com>

scripts/imgtool/main.py
@@ -377,6 +377,7 @@ class BasedIntParamType(click.ParamType):
               'keyword to automatically generate it from the image version.')
 @click.option('-v', '--version', callback=validate_version,  required=True)
 @click.option('--align', type=click.Choice(['1', '2', '4', '8', '16', '32']),
+              default='1',
               required=False,
               help='Alignment used by swap update modes.')
 @click.option('--max-align', type=click.Choice(['8', '16', '32']),