183b27e9d57004d5b8251a15cd54605591bc7862
Bram Vlerick <bram.vlerick@openpixelsystems.org>
2024-03-08 15:05:16 +0100

    imgtool: dumpinfo.py: use correct define when tlv iterating
    When iterating over tlv's the `TLV_SIZE` should be used and not
    `TLV_INFO_SIZE`.
    
    Signed-off-by: Bram Vlerick <bram.vlerick@openpixelsystems.org>

--- scripts/imgtool/dumpinfo.py
+++ scripts/imgtool/dumpinfo.py
@@ -168,7 +168,7 @@ def dump_imginfo(imgfile, outfile=None, silent=False):
             tlv_type, tlv_len = struct.unpack(
                 'HH',
                 b[tlv_off:(tlv_off + image.TLV_INFO_SIZE)])
-            tlv_off += image.TLV_INFO_SIZE
+            tlv_off += image.TLV_SIZE
             tlv_data = b[tlv_off:(tlv_off + tlv_len)]
             tlv_area["tlvs_prot"].append(
                 {"type": tlv_type, "len": tlv_len, "data": tlv_data})
@@ -186,7 +186,7 @@ def dump_imginfo(imgfile, outfile=None, silent=False):
         tlv_type, tlv_len = struct.unpack(
             'HH',
             b[tlv_off:(tlv_off + image.TLV_INFO_SIZE)])
-        tlv_off += image.TLV_INFO_SIZE
+        tlv_off += image.TLV_SIZE
         tlv_data = b[tlv_off:(tlv_off + tlv_len)]
         tlv_area["tlvs"].append(
             {"type": tlv_type, "len": tlv_len, "data": tlv_data})
