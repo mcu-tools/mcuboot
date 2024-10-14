b9d69dd2a2d6df32da6608d549138288bb7d7aa5
Toon Stegen <toon@toostsolutions.be>
2024-10-14 11:33:06 +0200

    zephyr: print version number before boot
    Signed-off-by: Toon Stegen <toon@toostsolutions.be>

--- boot/zephyr/main.c
+++ boot/zephyr/main.c
@@ -553,6 +553,10 @@ int main(void)
                  rsp.br_image_off);
 #endif
 
+    BOOT_LOG_INF("Image version: v%d.%d.%d", rsp.br_hdr->ih_ver.iv_major,
+                                                    rsp.br_hdr->ih_ver.iv_minor,
+                                                    rsp.br_hdr->ih_ver.iv_revision);
+
 #if defined(MCUBOOT_DIRECT_XIP)
     BOOT_LOG_INF("Jumping to the image slot");
 #else
