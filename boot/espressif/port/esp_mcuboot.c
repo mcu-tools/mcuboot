6b07b98770ec6a62151f2e6812f02197f1db1812
Martin Jäger <martin@libre.solar>
2025-01-05 13:35:32 +0100

    boot: espressif: add support for upgrade modes w/o scratch
    Upgrade using move decreases flash wear and is now the default mode
    used by Zephyr.
    
    The implementation is done by MCUboot itself, so it is just a matter of
    setting the correct define to enable it.
    
    Swap using move was successfully tested with ESP32C6.
    
    Signed-off-by: Martin Jäger <martin@libre.solar>

--- boot/espressif/port/esp_mcuboot.c
+++ boot/espressif/port/esp_mcuboot.c
@@ -15,6 +15,7 @@
 #include "esp_err.h"
 #include "bootloader_flash_priv.h"
 #include "esp_flash_encrypt.h"
+#include "mcuboot_config/mcuboot_config.h"
 
 #include "flash_map_backend/flash_map_backend.h"
 #include "sysflash/sysflash.h"
@@ -49,16 +50,19 @@ _Static_assert(IS_ALIGNED(FLASH_BUFFER_SIZE, 4), "Buffer size for SPI Flash oper
 
 #define BOOTLOADER_START_ADDRESS CONFIG_BOOTLOADER_OFFSET_IN_FLASH
 #define BOOTLOADER_SIZE CONFIG_ESP_BOOTLOADER_SIZE
+
 #define IMAGE0_PRIMARY_START_ADDRESS CONFIG_ESP_IMAGE0_PRIMARY_START_ADDRESS
 #define IMAGE0_SECONDARY_START_ADDRESS CONFIG_ESP_IMAGE0_SECONDARY_START_ADDRESS
-#define SCRATCH_OFFSET CONFIG_ESP_SCRATCH_OFFSET
 #if (MCUBOOT_IMAGE_NUMBER == 2)
 #define IMAGE1_PRIMARY_START_ADDRESS CONFIG_ESP_IMAGE1_PRIMARY_START_ADDRESS
 #define IMAGE1_SECONDARY_START_ADDRESS CONFIG_ESP_IMAGE1_SECONDARY_START_ADDRESS
 #endif
-
 #define APPLICATION_SIZE CONFIG_ESP_APPLICATION_SIZE
+
+#ifdef CONFIG_ESP_BOOT_SWAP_USING_SCRATCH
+#define SCRATCH_OFFSET CONFIG_ESP_SCRATCH_OFFSET
 #define SCRATCH_SIZE CONFIG_ESP_SCRATCH_SIZE
+#endif
 
 extern int ets_printf(const char *fmt, ...);
 
@@ -99,12 +103,14 @@ static const struct flash_area secondary_img1 = {
 };
 #endif
 
+#ifdef CONFIG_ESP_BOOT_SWAP_USING_SCRATCH
 static const struct flash_area scratch_img0 = {
     .fa_id = FLASH_AREA_IMAGE_SCRATCH,
     .fa_device_id = FLASH_DEVICE_INTERNAL_FLASH,
     .fa_off = SCRATCH_OFFSET,
     .fa_size = SCRATCH_SIZE,
 };
+#endif
 
 static const struct flash_area *s_flash_areas[] = {
     &bootloader,
@@ -114,7 +120,9 @@ static const struct flash_area *s_flash_areas[] = {
     &primary_img1,
     &secondary_img1,
 #endif
+#ifdef CONFIG_ESP_BOOT_SWAP_USING_SCRATCH
     &scratch_img0,
+#endif
 };
 
 static const struct flash_area *prv_lookup_flash_area(uint8_t id) {
