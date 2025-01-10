ed48911951ad6fb20ae904d28d5b2b66600f1a9d
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2025-01-10 11:42:38 +0000

    boot: bootutil: loader: Fix some comments
    Fixes some comments which had typos or were not formatted correctly
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- boot/bootutil/src/loader.c
+++ boot/bootutil/src/loader.c
@@ -787,8 +787,9 @@ boot_image_check(struct boot_loader_state *state, struct image_header *hdr,
     (void)bs;
     (void)rc;
 
-/* In the case of ram loading the image has already been decrypted as it is
- * decrypted when copied in ram */
+    /* In the case of ram loading the image has already been decrypted as it is
+     * decrypted when copied in ram
+     */
 #if defined(MCUBOOT_ENC_IMAGES) && !defined(MCUBOOT_RAM_LOAD)
     if (MUST_DECRYPT(fap, BOOT_CURR_IMG(state), hdr)) {
         rc = boot_enc_load(state, 1, hdr, fap, bs);
@@ -1347,7 +1348,7 @@ boot_copy_region(struct boot_loader_state *state,
 
 #ifdef MCUBOOT_ENC_IMAGES
         /* If only copy, then does not matter if header indicates need for
-         * encryptio/decryptio, we just copy data. */
+         * encryption/decryption, we just copy data. */
         if (!only_copy && IS_ENCRYPTED(hdr)) {
 #if defined(MCUBOOT_SWAP_USING_OFFSET)
             uint32_t abs_off = off - sector_off + bytes_copied;
