8148044d267d6e22c69c8d5e0146976258150b72
Maulik Patel <maulik.patel@arm.com>
2025-05-16 06:56:56 +0100

    imgtool: Rename key-ids to psa-key-ids
    Since the key id concept in the PSA specific, rename the variables
    accordingly.
    
    Signed-off-by: Maulik Patel <maulik.patel@arm.com>
    Change-Id: I8a8a5ceba5554211f185cc4045a6081b6d407507
    (cherry picked from commit 7658d57e6b6e521a45449e687e463cd71d0e1a78)

--- scripts/imgtool/image.py
+++ scripts/imgtool/image.py
@@ -306,7 +306,7 @@ class Image:
 
         self.image_hash = None
         self.image_size = None
-        self.signature = None
+        self.signatures = None
         self.version = version or versmod.decode_version("0")
         self.header_size = header_size
         self.pad_header = pad_header
@@ -331,7 +331,7 @@ class Image:
         self.non_bootable = non_bootable
         self.vid = vid
         self.cid = cid
-        self.key_ids = None
+        self.psa_key_ids = None
 
         if self.max_align == DEFAULT_MAX_ALIGN:
             self.boot_magic = bytes([
@@ -742,9 +742,9 @@ class Image:
             self.signatures = []
             for i, key in enumerate(keys):
                 # If key IDs are provided, and we have enough for this key, add it first.
-                if self.key_ids is not None and len(self.key_ids) > i:
+                if self.psa_key_ids is not None and len(self.psa_key_ids) > i:
                     # Convert key id (an integer) to 4-byte big-endian bytes.
-                    kid_bytes = self.key_ids[i].to_bytes(4, 'big')
+                    kid_bytes = self.psa_key_ids[i].to_bytes(4, 'big')
                     tlv.add('KEYID', kid_bytes)  # Using the TLV tag that corresponds to key IDs.
 
                 if public_key_format == 'hash':
@@ -1033,9 +1033,9 @@ class Image:
             return VerifyResult.INVALID_SIGNATURE, None, None, None
 
 
-    def set_key_ids(self, key_ids):
+    def set_key_ids(self, psa_key_ids):
         """Set list of key IDs (integers) to be inserted before each signature."""
-        self.key_ids = key_ids
+        self.psa_key_ids = psa_key_ids
 
     def _add_key_id_tlv_to_unprotected(self, tlv, key_id: int):
         """Add a key ID TLV into the *unprotected* TLV area."""
