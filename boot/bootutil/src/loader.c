fb22bdf5e441e3dbf13c4a4ad50c92a574768a65
Almir Okato <almir.okato@espressif.com>
2025-05-24 14:34:38 -0300

    DEBUG: add debug logs - THIS COMMIT WILL BE DELETED
    Signed-off-by: Almir Okato <almir.okato@espressif.com>

--- boot/bootutil/src/loader.c
+++ boot/bootutil/src/loader.c
@@ -49,6 +49,9 @@
 #include "bootutil/boot_hooks.h"
 #include "bootutil/mcuboot_status.h"
 
+// TEMP FOR DEBUG - REMOVE
+#include "bootloader_flash_priv.h"
+
 #ifdef MCUBOOT_ENC_IMAGES
 #include "bootutil/enc_key.h"
 #endif
@@ -758,12 +761,47 @@ boot_write_status(const struct boot_loader_state *state, struct boot_status *bs)
                  (unsigned long)flash_area_get_off(fap) + off);
 
 #ifdef MCUBOOT_FLASH_HAS_HW_ENCRYPTION
+    BOOT_LOG_INF("loader.c writing SWAP STATUS; fa_id=%d off=0x%lx (0x%lx)",
+        flash_area_get_id(fap), (unsigned long)off,
+        (unsigned long)(flash_area_get_off(fap) + off));
     rc = flash_area_erase(fap, off, align);
+
+    // TEMP FOR DEBUG - REMOVE
+    uint8_t read_buf[BOOT_MAX_ALIGN];
+    rc = flash_area_read(fap, off, read_buf, BOOT_MAX_ALIGN);
+    BOOT_LOG_INF("whats read BEFORE writing rc=0x%x", rc);
+    for (uint8_t i=0; i<BOOT_MAX_ALIGN; i=i+4) {
+        BOOT_LOG_INF("0x%x 0x%x 0x%x 0x%x",
+            read_buf[i], read_buf[i+1], read_buf[i+2], read_buf[i+3]);
+    }
+    rc = bootloader_flash_read(fap->fa_off + off, read_buf, BOOT_MAX_ALIGN, false);
+    BOOT_LOG_INF("whats read BEFORE writing - RAW rc=0x%x", rc);
+    for (uint8_t i=0; i<BOOT_MAX_ALIGN; i=i+4) {
+        BOOT_LOG_INF("0x%x 0x%x 0x%x 0x%x",
+            read_buf[i], read_buf[i+1], read_buf[i+2], read_buf[i+3]);
+    }
+    // TEMP FOR DEBUG - REMOVE
+
 #endif
     rc = flash_area_write(fap, off, buf, align);
     if (rc != 0) {
         rc = BOOT_EFLASH;
     }
+
+    // TEMP FOR DEBUG - REMOVE
+    rc = flash_area_read(fap, off, read_buf, BOOT_MAX_ALIGN);
+    BOOT_LOG_INF("whats read AFTER writing rc=0x%x", rc);
+    for (uint8_t i=0; i<BOOT_MAX_ALIGN; i=i+4) {
+        BOOT_LOG_INF("0x%x 0x%x 0x%x 0x%x",
+            read_buf[i], read_buf[i+1], read_buf[i+2], read_buf[i+3]);
+    }
+    rc = bootloader_flash_read(fap->fa_off + off, read_buf, BOOT_MAX_ALIGN, false);
+    BOOT_LOG_INF("whats read AFTER writing - RAW rc=0x%x", rc);
+    for (uint8_t i=0; i<BOOT_MAX_ALIGN; i=i+4) {
+        BOOT_LOG_INF("0x%x 0x%x 0x%x 0x%x",
+            read_buf[i], read_buf[i+1], read_buf[i+2], read_buf[i+3]);
+    }
+    // TEMP FOR DEBUG - REMOVE
 
     flash_area_close(fap);
 
