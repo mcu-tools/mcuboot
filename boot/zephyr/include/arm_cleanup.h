5a728be97ae032b19845b5351acea577c8cee917
Mika Braunschweig <mika.braunschweig@siemens.com>
2025-06-13 15:45:41 +0200

    boot: zephyr: Add support for Cortex-R cleanup before final jump
    Add code to cleanup the Cortex-R processor state to the reset
    configuration and disable + acknowledge all interrupts before entering
    the booted application.
    
    Signed-off-by: Mika Braunschweig <mika.braunschweig@siemens.com>

--- boot/zephyr/include/arm_cleanup.h
+++ boot/zephyr/include/arm_cleanup.h
@@ -11,7 +11,7 @@
 /**
  * Cleanup interrupt priority and interupt enable registers.
  */
-void cleanup_arm_nvic(void);
+void cleanup_arm_interrupts(void);
 
 #if defined(CONFIG_CPU_HAS_ARM_MPU) || defined(CONFIG_CPU_HAS_NXP_SYSMPU)
 /**
