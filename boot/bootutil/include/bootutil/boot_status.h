tomchy/sdk-mcuboot

bootutil: Expose hardware security counter
Add a possibility to read the current value of the hardware security
counter from the application.

Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

boot/bootutil/include/bootutil/boot_status.h
@@ -119,6 +119,11 @@ extern "C" {
 #define BLINFO_MAX_APPLICATION_SIZE_IMAGE_2 0x07
 #define BLINFO_MAX_APPLICATION_SIZE_IMAGE_3 0x08
 #define BLINFO_MAX_APPLICATION_SIZE_IMAGE_4 0x09
+#define BLINFO_SECURITY_COUNTER_IMAGE_0 0x0a
+#define BLINFO_SECURITY_COUNTER_IMAGE_1 0x0b
+#define BLINFO_SECURITY_COUNTER_IMAGE_2 0x0c
+#define BLINFO_SECURITY_COUNTER_IMAGE_3 0x0d
+#define BLINFO_SECURITY_COUNTER_IMAGE_4 0x0e
 
 enum mcuboot_mode {
     MCUBOOT_MODE_SINGLE_SLOT,

mcu-tools/mcuboot

boot/bootutil/include/bootutil/boot_status.h
@@ -119,6 +119,11 @@ extern "C" {
 #define BLINFO_MAX_APPLICATION_SIZE_IMAGE_2 0x07
 #define BLINFO_MAX_APPLICATION_SIZE_IMAGE_3 0x08
 #define BLINFO_MAX_APPLICATION_SIZE_IMAGE_4 0x09
+#define BLINFO_SECURITY_COUNTER_IMAGE_0 0x10
+#define BLINFO_SECURITY_COUNTER_IMAGE_1 0x11
+#define BLINFO_SECURITY_COUNTER_IMAGE_2 0x12
+#define BLINFO_SECURITY_COUNTER_IMAGE_3 0x13
+#define BLINFO_SECURITY_COUNTER_IMAGE_4 0x14
 
 enum mcuboot_mode {
     MCUBOOT_MODE_SINGLE_SLOT,