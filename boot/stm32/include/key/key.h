4a0500a852c56ddeada6d3d12bbeb6f61f1af32f
le noa, emilien <emilien.lenoa@wika.com>
2025-10-21 15:31:33 +0200

    delete stm32 support

--- boot/stm32/include/key/key.h
+++
@@ -1,7 +0,0 @@
-#ifndef KEY_H_
-#define KEY_H_
-
-extern unsigned char enc_priv_key[];
-extern unsigned int enc_priv_key_len;
-
-#endif /* KEY_KEY_H_ */
--- boot/stm32/include/mcuboot_config/mcuboot_rng.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MCUBOOT_RNG_H__
-#define __MCUBOOT_RNG_H__
-
-#include "rng_stm32.h"
-
-#define MCUBOOT_RNG(...)  generator_rng_stm32(__VA_ARGS__)
-
-
-#endif /* __MCUBOOT_RNG_H__ */
