a312656b60c8af0572b7b45e21d53c748908eb69
Dominik Ermel <dominik.ermel@nordicsemi.no>
2025-10-24 14:27:31 +0000

    bootutil: Add boot_loader_state_init
    boot_loader_state initialization function.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- boot/bootutil/src/bootutil_loader.c
+++ boot/bootutil/src/bootutil_loader.c
@@ -381,3 +381,21 @@ boot_close_all_flash_areas(struct boot_loader_state *state)
     }
 }
 #endif /* !MCUBOOT_SINGLE_APPLICATION_SLOT_RAM_LOAD && !MCUBOOT_SINGLE_APPLICATION_SLOT */
+
+void boot_state_init(struct boot_loader_state *state)
+{
+#if defined(MCUBOOT_ENC_IMAGES)
+    int image;
+    int slot;
+#endif
+
+    memset(state, 0, sizeof(*state));
+
+#if defined(MCUBOOT_ENC_IMAGES)
+    for (image = 0; image < BOOT_IMAGE_NUMBER; ++image) {
+        for (slot = 0; slot < BOOT_NUM_SLOTS; ++slot) {
+            boot_enc_init(&state->enc[image][slot]);
+        }
+    }
+#endif
+}
