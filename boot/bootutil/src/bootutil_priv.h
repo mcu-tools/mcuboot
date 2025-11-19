7cdb50ccb8675ae8b05000cfbaff1a84fc93a514
Dominik Ermel <dominik.ermel@nordicsemi.no>
2025-11-19 17:14:45 +0000

    bootutil: Replace bootutil_verify_img with bootutil_verify_sig
    With small changes the bootutil_verify_sig can now be used
    for the same purpose as bootutil_verify_img.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- boot/bootutil/src/bootutil_priv.h
+++ boot/bootutil/src/bootutil_priv.h
@@ -281,10 +281,13 @@ struct boot_sector_buffer {
 #endif
 };
 
-/* The function is intended for verification of image hash against
- * provided signature.
+/* The function is intended for verification of message hash against
+ * provided signature. If MCUBOOT_SIGN_PURE is enabled the function
+ * expects msg to point to image to verify signature over, and mlen
+ * is image size; otherwise msg is expected  to be pointer to hash of
+ * an image and mlen to length of the hash.
  */
-fih_ret bootutil_verify_sig(uint8_t *hash, uint32_t hlen, uint8_t *sig,
+fih_ret bootutil_verify_sig(uint8_t *msg, uint32_t mlen, uint8_t *sig,
                             size_t slen, uint8_t key_id);
 
 /* The function is intended for direct verification of image
