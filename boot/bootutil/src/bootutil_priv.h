0acc980611bf359e868276f7359c06630347131a
Dominik Ermel <dominik.ermel@nordicsemi.no>
2025-12-19 14:51:03 +0000

    bootutil: boot_read_enc_key now returns boolean
    No need to return error code we can not do anything with.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- boot/bootutil/src/bootutil_priv.h
+++ boot/bootutil/src/bootutil_priv.h
@@ -339,8 +339,8 @@ bool boot_status_is_reset(const struct boot_status *bs);
 
 #ifdef MCUBOOT_ENC_IMAGES
 int boot_write_enc_keys(const struct flash_area *fap, const struct boot_status *bs);
-int boot_read_enc_key(const struct flash_area *fap, uint8_t slot,
-                      struct boot_status *bs);
+bool boot_read_enc_key(const struct flash_area *fap, uint8_t slot,
+                       struct boot_status *bs);
 #endif
 
 /**
