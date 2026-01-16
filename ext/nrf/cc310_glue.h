65d7f3d3124c24180e1bd86c2c6ef2625cee7d9a
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2026-01-16 09:46:00 +0000

    ext: nrf: cc310_glue: Fix include path
    Zephyr include paths without the "zephyr" prefix were deprecated
    a long time ago, fix this include path
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- ext/nrf/cc310_glue.h
+++ ext/nrf/cc310_glue.h
@@ -22,7 +22,7 @@
 #include <nrf_cc310_bl_init.h>
 #include <nrf_cc310_bl_hash_sha256.h>
 #include <nrf_cc310_bl_ecdsa_verify_secp256r1.h>
-#include <devicetree.h>
+#include <zephyr/devicetree.h>
 #include <string.h>
 
 /*
