7abadfad73268ded75b8f83cce3e96a36bda2a18
Daniel Stindl <daniel.stindl@nxp.com>
2026-04-09 12:59:22 +0200

    boot: bootutil: Add a hook for implement image's pre-copying action
    boot_copy_region_pre_hook() runs right before an image is copied into
    the primary slot in MCUBOOT_OVERWRITE_ONLY and swap modes. Supply the
    size of the region to be copied to the hook so vendor implementations
    can size their pre-copy work.
    
    This hook placement is ideal for vendors with dedicated hardware
    encryption engines who want to implement encrypted XIP. Such engines
    typically cannot use the SWAP upgrade mode, making OVERWRITE_ONLY the
    only viable upgrade strategy. Before an image is (re-)encrypted into the
    primary slot, the engine usually needs to be initialized based on the
    source image size. In addition, some engines implicitly consume more
    physical memory than the plain image, so extra primary-slot flash
    sectors must be pre-erased before the update.
    
    Signed-off-by: Daniel Stindl <daniel.stindl@nxp.com>

--- boot/bootutil/src/loader.c
+++ boot/bootutil/src/loader.c
@@ -946,11 +946,18 @@ boot_copy_image(struct boot_loader_state *state, struct boot_status *bs)
 
     (void)bs;
 
-#if defined(MCUBOOT_OVERWRITE_ONLY_FAST) || defined(MCUBOOT_SWAP_USING_MOVE) || defined(MCUBOOT_SWAP_USING_OFFSET)
+#if defined(MCUBOOT_OVERWRITE_ONLY_FAST) || defined(MCUBOOT_SWAP_USING_MOVE) || defined(MCUBOOT_SWAP_USING_OFFSET) || defined(MCUBOOT_IMAGE_ACCESS_HOOKS)
     uint32_t src_size = 0;
     rc = boot_read_image_size(state, BOOT_SLOT_SECONDARY, &src_size);
     assert(rc == 0);
 #endif
+
+    rc = BOOT_HOOK_CALL(boot_copy_region_pre_hook, 0, BOOT_CURR_IMG(state),
+                        BOOT_IMG_AREA(state, BOOT_SLOT_PRIMARY),
+                        BOOT_IMG_AREA(state, BOOT_SLOT_SECONDARY), src_size);
+    if (rc != 0) {
+        return rc;
+    }
 
     image_index = BOOT_CURR_IMG(state);
 
