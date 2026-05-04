4eb84ba21ae0dd4d530d155a9af715038c2957a4
Almir Okato <almir.okato@espressif.com>
2026-05-04 10:33:32 -0300

    espressif: ESP32-C61 initial support
    Add initial support for ESP32-C61
    
    Signed-off-by: Almir Okato <almir.okato@espressif.com>

---
+++ boot/espressif/tools/toolchain-esp32c61.cmake
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2026 Espressif Systems (Shanghai) CO LTD
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set(CMAKE_SYSTEM_NAME Generic)
+
+set(CMAKE_C_COMPILER riscv32-esp-elf-gcc)
+set(CMAKE_CXX_COMPILER riscv32-esp-elf-g++)
+set(CMAKE_ASM_COMPILER riscv32-esp-elf-gcc)
+
+set(CMAKE_C_FLAGS "-march=rv32imac_zicsr_zifencei_zaamo_zalrsc" CACHE STRING "C Compiler Base Flags")
+set(CMAKE_CXX_FLAGS "-march=rv32imac_zicsr_zifencei_zaamo_zalrsc" CACHE STRING "C++ Compiler Base Flags")
+set(CMAKE_EXE_LINKER_FLAGS "-nostartfiles -march=rv32imac_zicsr_zifencei_zaamo_zalrsc --specs=nosys.specs" CACHE STRING "Linker Base Flags")
